38434cc6fd83b515dd5c79bd5af9558bf57ab770
asaplogic <marogil.andy@me.com>
2016-07-26 23:28:53 -0500

    Add files via upload

--- playlist.py
+++ playlist.py
@@ -13,12 +13,18 @@ def pull_data(playlist):
 
     playlist_url = "http://www.radiomilwaukee.org.php56-15.dfw3-2.websitetestlink.com/playerstream/getstream.php?history"
     list_code = requests.get(playlist_url).text
-    
+    list_code = str(list_code)
     
 
+
+    print("Fetching playlists from " + file_name + "...\n")
+    
+    for line in list_code:
+            title,artist,album,time = line.split(',')
+
+            print(title)
+    
+   
 
 
 pull_data(list)
-
-block_list = list_code.split()
-print(block_list)
